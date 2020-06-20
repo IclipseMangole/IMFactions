cca606846adb17533fadf81ff0cb00ff7a3514b2
Iclipse <iclipskii@gmail.com>
2020-06-20 21:45:30 +0200

    Updated

---
+++ src/main/java/de/imfactions/functions/items/FactionItem.java
@@ -0,0 +1,13 @@
+package de.imfactions.functions.items;
+
+//   |    ----  |       |   |---  -----  |---
+//   |   |      |       |   |  |  |      |
+//   |   |      |       |   |--   -----  |---
+//   |   |      |       |   |         |  |
+//   |    ----   ----   |   |     -----  |---
+
+/**
+ * Created by Iclipse on 20.06.2020
+ */
+public class FactionItem {
+}
