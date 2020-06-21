08f8f5d4dd901568c4a5f7e4f0747796a2e68d3e
Iclipse <iclipskii@gmail.com>
2020-06-21 21:08:33 +0200

    Added LocationBuilder.java

---
+++ src/main/java/de/imfactions/util/LocationBuilder.java
@@ -0,0 +1,44 @@
+package de.imfactions.util;
+
+//   |    ----  |       |   |---  -----  |---
+//   |   |      |       |   |  |  |      |
+//   |   |      |       |   |--   -----  |---
+//   |   |      |       |   |         |  |
+//   |    ----   ----   |   |     -----  |---
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+/**
+ * Created by Iclipse on 21.06.2020
+ */
+public class LocationBuilder {
+    public static String toString(Location location) {
+        String loc = "";
+
+        loc = loc + location.getWorld().getName();
+        loc = loc + "," + location.getX();
+        loc = loc + "," + location.getY();
+        loc = loc + "," + location.getZ();
+        if (location.getYaw() == 0 && location.getPitch() == 0) {
+            loc = loc + "," + location.getYaw();
+            loc = loc + "," + location.getPitch();
+        }
+
+        return loc;
+    }
+
+
+    public static Location fromString(String location) {
+        Location loc = null;
+        String[] sloc = location.split(",");
+        if (sloc.length == 4) {
+            loc = new Location(Bukkit.getWorld(sloc[0]), Double.valueOf(sloc[1]), Double.valueOf(sloc[2]), Double.valueOf(sloc[3]));
+        }
+        if (sloc.length == 6) {
+            loc = new Location(Bukkit.getWorld(sloc[0]), Double.valueOf(sloc[1]), Double.valueOf(sloc[2]), Double.valueOf(sloc[3]), Float.valueOf(sloc[4]), Float.valueOf(sloc[5]));
+        }
+        return loc;
+    }
+
+}
