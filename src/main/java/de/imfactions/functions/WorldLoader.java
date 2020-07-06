09763cedc31bc07aeb6f743ae5fae50e062a441d
Yannick <iclipskii@gmail.com>
2020-07-06 22:28:54 +0200

    1.16.1 update

--- src/main/java/de/imfactions/functions/WorldLoader.java
+++ src/main/java/de/imfactions/functions/WorldLoader.java
@@ -8,6 +8,7 @@ package de.imfactions.functions;
 
 import de.imfactions.IMFactions;
 import org.bukkit.craftbukkit.libs.org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,24 +18,30 @@ import java.nio.file.StandardCopyOption;
  * Created by Iclipse on 21.06.2020
  */
 public class WorldLoader {
-    public static void loadLobby() {
+    private IMFactions factions;
+
+    public WorldLoader(IMFactions factions) {
+        this.factions = factions;
+    }
+
+    public void loadLobby() {
         //if (mapUpdate) {
         File from = new File("/home/IMNetzwerk/BuildServer/FactionLobby_world/region");
-        File to = new File(IMFactions.getInstance().getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world/region");
+        File to = new File(factions.getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world/region");
 
         try {
             if (to.exists()) {
-                FileUtils.deleteDirectory(new File(IMFactions.getInstance().getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world"));
+                FileUtils.deleteDirectory(new File(factions.getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world"));
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         //if (to.getTotalSpace() != from.getTotalSpace()) {
 
         try {
             FileUtils.copyDirectory(from, to);
-            Files.copy(new File("/home/IMNetzwerk/BuildServer/FactionLobby_world/level.dat").toPath(), new File(IMFactions.getInstance().getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world/level.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
-            new File(IMFactions.getInstance().getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world/data").mkdir();
+            Files.copy(new File("/home/IMNetzwerk/BuildServer/FactionLobby_world/level.dat").toPath(), new File(factions.getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world/level.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+            new File(factions.getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath() + "/world/data").mkdir();
         } catch (IOException e) {
             e.printStackTrace();
         }
